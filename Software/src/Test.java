75ec36ffa11b226175022f90b1089b80606f808e
sukeshratnalu <sukesh.ratnalu@india.semanticbits.com>
2018-03-05 19:39:30 +0530

    test

--- Software/src/Test.java
+++ Software/src/Test.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Test {
+    static String name ;
+    String getData(String userName){
+        String data = userName;
+        return data;
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello Sukesh");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        Test obj = new Test();
+        System.out.println("User entered " + obj.getData(name));
     }
 }
